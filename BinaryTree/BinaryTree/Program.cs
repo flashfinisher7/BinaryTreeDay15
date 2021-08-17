a7571af21e3efe5ae73a24104fdcedd6ca8041f4
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-17 15:21:48 +0530

    Refactor:Creating BST and Adding input

--- BinaryTree/BinaryTree/Program.cs
+++ BinaryTree/BinaryTree/Program.cs
@@ -7,6 +7,11 @@ namespace BinaryTree
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t This is Binary Search Tree");
+            BTree<int> binarySearch = new BTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
         }
     }
 }
